64171ea2d9e896476637ccabd564b68bdf6e6b9a
laura <lauralekeu13@gmail.com>
2020-11-05 15:04:30 +0100

    Compteur des tâches restantes
    Affichage du nombre de tâches restantes

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
 "use strict";
 
 window.onload = function() { // window.onload = Evènement particulier qui s'assure que le DOM est complétement chargé
-
-  // CAPTURER LES ELEMENTS
+/* -----------------------------------------------------------------------------
+      CAPTURER LES ELEMENTS
+-------------------------------------------------------------------------------- */
   const newTacheInput = document.querySelector('.new-todo');
   const listeTaches = document.querySelector('.todo-list');
 
+  const compteurTaches = document.getElementById('todo-count');
 
-  // FONCTION AJOUT D'UNE TACHE :
-  function addItem(element) {
+
+/* -----------------------------------------------------------------------------
+      FONCTIONS
+-------------------------------------------------------------------------------- */
+
+/**
+ * [AJOUT D'UNE TÂCHE]
+ * @param {[type]} nouvelleTache [description]
+ */
+  function addItem(nouvelleTache) {
     // Créer un nouveau li:
     const newLi = document.createElement('li');
     newLi.classList.add('listItem');
     newLi.classList.add('cache'); // Cacher la nouvelle tache (app.css)
     newLi.innerHTML = `
       <input class="toggle" type="checkbox" />
-      <label> ${element.value} </label>
+      <label> ${nouvelleTache.value} </label>
       <button class="destroy"></button>
     `;
     // Ajouter la new tâche au début de la liste avec un slideDown
@@ -24,14 +34,43 @@ window.onload = function() { // window.onload = Evènement particulier qui s'ass
       listeTaches.childNodes[0].classList.remove('cache'); // Virer la classe cache pour avoir l'animation
     });
     // Vider le champ de texte (newTacheInput)
-    element.value = '';
+    nouvelleTache.value = '';
+
+    // Lancer des fonctions
+    displayNotCompleted();  // Compteur de tâches
+  }
+
+/**
+ * [COMPTEUR DES TÂCHES RESTANTES]
+ * @return {[type]} [description]
+ */
+  function displayNotCompleted() {
+    compteurTaches.innerText = listeTaches.querySelectorAll('li:not(.completed)').length; // selectionner tous les li qui n'ont pas de classe completed
+
   }
 
 
-  // CAPTURE DES EVENEMENTS
+/* -----------------------------------------------------------------------------
+      CAPTURE DES EVENEMENTS
+-------------------------------------------------------------------------------- */
+
+// Appuyer sur la touche "Enter" pour ajouter la new tâche
   newTacheInput.addEventListener('keyup', function(e){
     if (e.keyCode === 13) {
       addItem(this); //
     }
   });
+
+
+
+
+/* -----------------------------------------------------------------------------
+      LANCEMENT DE FONCTIONS AU CHARGEMENT DE LA PAGE
+-------------------------------------------------------------------------------- */
+  displayNotCompleted();
+
+
+
+
+
 }
